0d80b67fddc16f5bdef2a0de6ff421b77a8f1366
VesnaK193 <vesna.karaklic.97@gmail.com>
2021-03-25 20:57:00 +0100

    Car Store - Console App (finished)

---
+++ CarClassLibrary/Car.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        public Car()
+        {
+            Make = "";
+            Model = "";
+            Price = 0.00M;
+        }
+
+        public Car(string make, string model, decimal price)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Make + " " + Model + " " + Price + "$";
+        }
+
+    }
+}
